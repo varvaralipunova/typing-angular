094e69f9037323283c98144b3994a1cdb4ae8656
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Air.local>
2022-03-25 23:12:46 -0700

    add styling for letters, and success message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { lorem } from 'faker'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
-}
+  title = 'typing'
+  sentence = lorem.sentence()
+  enteredText = ''
+
+  onInput(event: any) {
+    this.enteredText = event.target.value
+  }
+
+  compare(letter:string, enteredLetter:string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+
+    return letter === enteredLetter ? 'correct' : 'incorrect'
+  }
+} 
